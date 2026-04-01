5689b569bdd379d76d1bbf7f1b89400a99a9f570
TReeBOX <homzz7777@gmail.com>
2026-04-01 18:41:41 +0900

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -58,7 +58,7 @@ export const INITIAL_PORTFOLIO: PortfolioItem[] = [
   {
     "id": "5",
     "title": "Detailed Urban Diorama Model",
-    "category": "Exterior", // 'Blueprint' 대신 'Exterior'로 수정하여 에러 해결
+    "category": "Exterior", 
     "description": "건축을 2D로 표현하기 위한 최고의 블루프린트 AI이미지 입니다.",
     "images": ["/test-10.jpg", "/test-9.jpg"],
     "role": "Interior Visualization",
